42360df0dc3875d353a0376fd10804e692c3565c
AlexSawyer10 <alexvsawyer@gmail.com>
2025-06-23 18:23:13 -0400

    added sports

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ function App() {
 
   return (
     <>
+
+    <h1>MY NAME IS ALEX</h1>
+    <h2>I play soccer</h2>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
